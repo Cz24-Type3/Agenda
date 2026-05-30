b67bfa07ace2f8afda2ae6cdb7b5bd2ae8714431
Cz24-Type3 <joaopbruxo859@gmail.com>
2026-05-30 04:44:17 -0300

    APK Fix

---
+++ README.md
@@ -0,0 +1,15 @@
+Um aplicativo mobile que eu criei para testar as minhas habilidades.
+
+Nele você pode salvar, deletar e editar "Cards" de contatos.
+
+    Como usar?
+1- Instalas as dependências
+    npm install
+2- Rodas o Expo
+    npm start
+3- Rodar em Web ou em emulador
+    Pressionar W para rodar em web
+    Pressionar A para rodar no emulador
+
+Buildar APK
+    eas build 
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import 'expo-sqlite/localStorage/install'
+import Constants from 'expo-constants';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseKey);
